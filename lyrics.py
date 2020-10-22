0761ab44e948a686db8f600fb6b37613923be522
Aryaman Sharma <aryamansharma01>
2020-10-22 15:39:13 +0530

    Lyrics extraction and visualisation

--- lyrics.py
+++ lyrics.py
@@ -1,73 +1,41 @@
-import types
-import pandas as pd
-import json
-import requests
-import sys
-import spotipy
-import spotipy.util as util
-#from config.config import USERNAME, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRETSPOTIPY_REDIRECT_URI
+import re
+from wordcloud import WordCloud
+import nltk
+from nltk import sent_tokenize
+from nltk import word_tokenize
+from nltk import FreqDist
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import plotly.express as px
+import plotly.tools as tls
+import plotly.graph_objs as go
+import plotly.offline as py
 from bs4 import BeautifulSoup
-
-
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+import spotipy
+import sys
+import requests
+import json
+import lyricsgenius
+import types
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+genius = lyricsgenius.Genius('l4J-3WFatATy8mJHgnsYVausDrWZo9GsXzui4_DWu5PiXfMDZ_s30iAB2DGtdEMQ')
 # for acessing private playlists
 scope = 'playlist-read-private'
 username = '8eia8ggl4ipbhouhun62o9y8i'
+client_id = '3cb41450f466404399f3e0de3e4c89f2'
+client_secret = '51406619960a46c3a0fc8682e5152784'
+redirect_uri = 'http://localhost:8888/callback/'
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+token = util.prompt_for_user_token(username, scope, client_id, client_secret,
+                                   redirect_uri)
 
-token = util.prompt_for_user_token(username,
-                                   scope, client_id='3cb41450f466404399f3e0de3e4c89f2',
-                                   client_secret='51406619960a46c3a0fc8682e5152784',
-                                   redirect_uri='http://localhost:8888/callback/')
-
-# function for prinitng song name and lyrics
-
-
-def show_lyrics(tracks):
-    for i, item in enumerate(tracks['items']):
-        track = item['track']
-        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
-                                    track['name']))
-        artist = track['artists'][0]['name']
-        name = track['name']
-        # formatting song url for
-        song_url = '{}-{}-lyrics'.format(str(artist).strip().replace(' ', '-').replace('(', '').replace(')', ''),
-                                         str(name).strip().replace(' ', '-').replace('(', '').replace(')', ''))
-        print(song_url)
-
-        request = requests.get("https://genius.com/{}".format(song_url))
-        print(request.status_code)
-
-        if request.status_code == 200:
-            html_code = BeautifulSoup(request.text, features="html.parser")
-            p = html_code.find("div", {"class": "lyrics"})
-            if p is not None:
-                lyrics = p.get_text()
-                print(lyrics)
-                print("-----------")
-
-        else:
-            print("Sorry, I can't find the actual song")
-
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    playlists = sp.user_playlists(username)
-    for playlist in playlists['items']:
-        if playlist['owner']['id'] == username:
-            print()
-            print(playlist['name'])
-            print('  total tracks', playlist['tracks']['total'])
-            results = sp.playlist(playlist['id'],
-                                  fields="tracks,next")
-            tracks = results['tracks']
-
-            show_lyrics(tracks)
-            while tracks['next']:
-                tracks = sp.next(tracks)
-                show_tracks(tracks)
-
-
-else:
-    print("Can't get token for", username)
+# accessing last 50 tracks frpm history
 
 
 def current_user_recently_played(self, limit=50):
@@ -84,17 +52,65 @@ token = util.prompt_for_user_token(
 spotify = spotipy.Spotify(auth=token)
 spotify.current_user_recently_played = types.MethodType(current_user_recently_played, spotify)
 
+# creating .json file
 recentsongs = spotify.current_user_recently_played(limit=50)
-out_file = open("recentsongs.json", "w")
-out_file.write(json.dumps(recentsongs, sort_keys=True, indent=2))
-out_file.close()
+track_name = []
+album_name = []
+artist_name = []
+# creating arrays for storing id and name
+for i in recentsongs['items']:
+    track_name.append(i['track']['name'])
+    artist_name.append(i['track']['artists'][0]['name'])
+lyrics = {}
+text = []
+compoundscore = []
+sid = SentimentIntensityAnalyzer()
+track_name = list(dict.fromkeys(track_name))
+for i in range(len(track_name)):
+    song = genius.search_song(track_name[i], artist_name[i])
+    songlyrics = song.lyrics.replace("\n", " ").replace("\\'", "\'")
+    lyrics[track_name[i]] = songlyrics
+    songlyrics = songlyrics.replace('(', '').replace(')', '')
+    text.append(re.sub("[\\[].*?[\\]]", "", songlyrics))
+    scores = sid.polarity_scores(text[i])
+    compoundscore.append(scores['compound'])
+text = ' '.join(map(str, text))
+print(text.encode("utf-8"))
+stopwords = set(stopwords.words('english'))
+stopwords.update(["br", "href", "la", "yeah", "yuh", "wan", "i'm"])
 
-#print(json.dumps(recentsongs, sort_keys=True, indent=2))
-track = json.dumps(recentsongs, sort_keys=True, indent=2)
-f = open('recentsongs.json',)
-data = json.load(f)
-f.close()
-print(type(track))
-# print(data)
-# print(pd.read_json(tracks))
-show_lyrics(recentsongs)
+sentences = sent_tokenize(text)
+words = word_tokenize(text)
+words_no_punc = []
+for w in words:
+    if w.isalpha():
+        words_no_punc.append(w.lower())
+
+ps = PorterStemmer()
+clean_words = []
+for w in words_no_punc:
+    if w not in stopwords:
+        clean_words.append(ps.stem(w))
+fdist = FreqDist(clean_words)
+print(fdist.most_common(10))
+fdist.plot(10)
+
+# making wordcloud
+words_string = ' '.join(map(str, clean_words))
+wordcloud = WordCloud(stopwords=stopwords).generate(words_string)
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+print(compoundscore)
+plt.plot(compoundscore)
+plt.show()
+pos_count = 0
+neg_count = 0
+for num in compoundscore:
+    if num >= 0:
+        pos_count += 1
+
+    else:
+        neg_count += 1
+plt.pie([pos_count, neg_count], labels=["Positive Songs", "Negative Songs"])
+plt.show()
